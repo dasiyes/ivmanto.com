2a93a8b42d61650936b5b8319a58e19bcf254697
Nikolay Tonev <nikolay.tonev@gmail.com>
2025-08-26 17:31:16 +0200

    fixed style for the web app.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
+// vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
